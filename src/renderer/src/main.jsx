45a9e6bccf57ccfbbb70f467bb9c5fc3edbbd7df
hhyufan <1838248655@qq.com>
2025-04-05 22:19:27 +0800

    fix: 修复Monaco编辑器的图标渲染问题

--- src/renderer/src/main.jsx
+++ src/renderer/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App'
 import './index.scss'
 // 导入Monaco编辑器配置
 import './monaco-config'
+// 导入Monaco编辑器图标字体
+import './monaco-icons.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
